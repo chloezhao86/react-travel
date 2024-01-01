b4a0a5ceb4f27e29aec15533b01c436185a26a66
Chloe <chloezhao86@gmail.com>
2024-01-02 10:39:14 +1100

    Add ProductCollection and BussinessPartners components and mock data
    This commit introduces new components - ProductCollection and BussinessPartners - to the application UI. Also, mock data is added for the ProductCollection component. Changes include component creation, image and component import updates, and restructuring to organize these components.

---
+++ src/componenst/productCollection/ProductImage.tsx
@@ -0,0 +1,40 @@
+import { Image, Typography } from "antd";
+
+interface PropsType {
+  id: string | number;
+  size: "large" | "small";
+  imageSrc: string;
+  price: number | string;
+  title: string;
+}
+
+export const ProductImage = ({
+  id,
+  size,
+  imageSrc,
+  price,
+  title,
+}: PropsType) => {
+  console.log("ProductImage", {
+    id,
+    size,
+    imageSrc,
+    price,
+    title,
+  });
+  return (
+    <>
+      {size == "large" ? (
+        <Image src={imageSrc} height={285} width={490} />
+      ) : (
+        <Image src={imageSrc} height={120} width={240} />
+      )}
+      <div>
+        <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
+        <Typography.Text type="danger" strong>
+          $ {price}起
+        </Typography.Text>
+      </div>
+    </>
+  );
+};
